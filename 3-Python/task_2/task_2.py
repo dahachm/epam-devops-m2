e353b04e71fc84efb20d1705463b79b2c7614c45
Darya Chemiakina <beetora@gmail.com>
2021-04-07 17:53:49 +0300

    Update task_2.py
            Add ability to input user`s list of numbers
    
            In printEven:
                    now continue to process input list even if non-number
                    value is met
                    count non-number values and print its total number

--- 3-Python/task_2/task_2.py
+++ 3-Python/task_2/task_2.py
@@ -7,43 +7,63 @@
 def printEven(numbers):
 
     print('printEven:', end='\n\t')
-    try:
-        for i in numbers:
+    unprocessed = 0
+    for i in numbers:
+        try:
             if (i % 2 == 0):
                 print(i, end=' ')
             if (i == 254):
                 print()
                 return
-        print()
-        return
-    except Exception as errorMsg:
-        print('\nERROR:', end='')
-        print(errorMsg)
-        return    
+        except:
+            unprocessed += 1
+    print(f'\n\t{unprocessed} value(s) couldn`t be processed due to type mismatch!\n')        
+    return        
+
+def toList (input_str):
+    input_list = input_str.split(' ')
+    for i in range(0, len(input_list)):
+        try:
+            input_list[i] = float(input_list[i])
+        except:
+            print(input_list[i])
+            continue
+    return input_list        
 
 
-test_1 = [1, 2, 3, 4, 6, 456, 222, -2, 254, 10, 45]
-test_2 = '[1, 2, 3, 4, 6, 456, 222, -2, 254, 10, 45]'
-test_3 = [1, 2, 3, 4, 6, '456', 222, -2, 254, 10, 45]
-test_4 = [254, 1, 2, 3, 4, 6, 456, 222, -2, 10, 45]
-test_5 = [1, 2, 3, 4, 6, 456, 222, -2, 10, 45]
+test = input('Type your list of space-separated numbers or leave empty, if you want to run my tests: ')
 
-print('\n    --- Case #1 ---')
-print(f'Input: {test_1} (type: {type(test_1)})')
-printEven(test_1)
+if (test == ''):
+    test_1 = [1, 2, 3, 4, 6, 456, 222, -2, 254, 10, 45]
+    test_2 = '[1, 2, 3, 4, 6, 456, 222, -2, 254, 10, 45]'
+    test_3 = [1, 2, 3, 4, 6, '456', 222, -2, 254, 10, 45]
+    test_4 = [254, 1, 2, 3, 4, 6, 456, 222, -2, 10, 45]
+    test_5 = [1, 2, 3, 4, 6, 456, 222, -2, 10, 45]
 
-print('\n    --- Case #2 ---')
-print('Input: ', test_2, '(type: ', type(test_2),')')
-printEven(test_2)
+    print('\n    --- Case #1 ---')
+    print(f'Input: {test_1} (type: {type(test_1)})')
+    printEven(test_1)
 
-print('\n    --- Case #3 ---')
-print(f'Input: {test_3} (type: {type(test_3)})')
-printEven(test_3)
+    print('\n    --- Case #2 ---')
+    print('Input: ', test_2, '(type: ', type(test_2),')')
+    printEven(test_2)
 
-print('\n    --- Case #4 ---')
-print(f'Input: {test_4} (type: {type(test_4)})')
-printEven(test_4)
+    print('\n    --- Case #3 ---')
+    print(f'Input: {test_3} (type: {type(test_3)})')
+    printEven(test_3)
 
-print('\n    --- Case #5 ---')
-print(f'Input: {test_5} (type: {type(test_5)})')
-printEven(test_5)
+    print('\n    --- Case #4 ---')
+    print(f'Input: {test_4} (type: {type(test_4)})')
+    printEven(test_4)
+
+    print('\n    --- Case #5 ---')
+    print(f'Input: {test_5} (type: {type(test_5)})')
+    printEven(test_5)
+
+else:
+    
+    test = toList(test)
+    print('\n    --- User test ---')
+    print(f'Input: {test} (type: {type(test)})')
+    printEven(test)
+    
